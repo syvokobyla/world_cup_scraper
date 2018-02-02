60b76f65abd553dad1960025aa0ddee5896d3893
Svitlana Syvokon <svitlana.syvokon@gmail.com>
2018-02-02 17:54:15 +0200

    Fix table. Add comments and docstrings

--- main.py
+++ main.py
@@ -4,24 +4,21 @@ import time
 import requests
 import re
 import logging
+from datetime import datetime
+
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
-
 from selenium.webdriver.firefox.options import Options
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 
 class MarkupError(Exception): 
+    """Should be thrown if markup of page is not as it was expected."""
     pass
 
 
 class BaseCrawler:
-
+    """Interface class for crawlers."""
+    
     DEFAULT_PATH = None
     DEFAULT_START_URL = None
     
@@ -34,19 +31,45 @@ class BaseCrawler:
         """Scrape the website and return parsed table.
         """
         raise NotImplementedError()
+   
+    def _navigate(self, url, path):
+        """Navigate from url to necessary page using key words from list 'path'.
+        
+        Args:
+            url : string
+                Starting url.
+            path : list of strings
+                Words from this list are used to find and click next necessary
+                link. And so on. 
+                
+        Returns:
+            string, the link to necessary page.
+        """  
+        raise NotImplementedError()
 
     def _parse(self, soup):
+        """Parse the soup, find teams and scores.
+        
+        Args:
+            soup : BeautifulSoup
+                soup of web page with teams and scores.
+        Returns:
+            team => score dictionary or {}
+        """
         try:
+            #Find the class and tag name, where team name is stored.
             team_string = soup.find(string=re.compile("^\s*" + 'Germany' + "\s*$"))
             team_parent = team_string.find_parent()
             team_tag_class = team_parent['class']
             team_tag_name = team_parent.name
-
+            
+            #Find the class and tag name, where odd is stored.
             odd_string = soup.find(string=re.compile("^\s*\d*/\d*\s*$"))
             odd_parent = odd_string.find_parent()
             odd_tag_class = odd_parent['class']
             odd_tag_name = odd_parent.name
 
+            #Find all teams and their odds
             odds_list = soup.find_all(odd_tag_name, {"class": odd_tag_class})
             team_list = soup.find_all(team_tag_name, {"class": team_tag_class})
             odds = [odd.text.strip() for odd in odds_list]
@@ -59,19 +82,28 @@ class BaseCrawler:
 
 
 class HtmlCrawler(BaseCrawler):
+    """Crawler for simple html sites."""
     
     def scrape(self):
+        """Scrape the website and return parsed table.
+        """
+        #Navigate to necessary page. 
         url = self._navigate(self.start_url, self.path)
+        #Get soup of that page.
         soup = self._get_soup(url)
+        #Return parsed data.
         return self._parse(soup)
         
     def _navigate(self, url, path):
+         """Navigate from url to necessary page using key words from list 'path'.
+         """
         next_url = url
         for word in path:
             logging.debug("Navigate to %s", word)
             soup = self._get_soup(next_url)
+            #Trying to find <a/> containing word from path list. 
             a = soup.find("a", text=re.compile("^\s*" + word + "\s*$"))
-            # Trying to find <a> word <a/> in more complex situation
+            # Trying to find <a> word <a/> in more complex situation.
             if not a:
                 a_string = soup.find(string=re.compile("^\s*" + word + "\s*$"))
                 if a_string:
@@ -86,7 +118,10 @@ class HtmlCrawler(BaseCrawler):
         return next_url
     
     def _get_soup(self, url, timeout=10, headers=None):
+        """Get soup from url.
+        """
         if not headers:
+            #Headers to mimic browser and not be recognized as bot.
             headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) '
                        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
         r = requests.get(url, timeout=timeout, headers=headers)
@@ -96,12 +131,13 @@ class HtmlCrawler(BaseCrawler):
 
 
 class JavascriptCrawler(BaseCrawler):
+    """Crawler for js sites."""
     
     def __init__(self, start_url=None, path=None):
-        super(JavascriptCrawler).__init__(start_url, path)
+        super().__init__(start_url, path)
 
-        firefox_options = Options()  
-        #firefox_options.add_argument("--headless") 
+        firefox_options = Options()
+        firefox_options.add_argument("--headless") 
         self.driver = webdriver.Firefox(firefox_options=firefox_options)
     
     def scrape(self):
@@ -112,8 +148,12 @@ class JavascriptCrawler(BaseCrawler):
         return self._parse(soup)
 
     def _navigate(self, url, path):
+         """Navigate from url to necessary page using key words from list 'path'.
+         """
         driver = self.driver
         driver.get(url)
+        #Execute the js script which looks for element with
+        #necessary text and click on it.
         driver.execute_script("""
             window.getLinkByText = (str) => { 
                 return Array.prototype.slice.call(
@@ -128,9 +168,10 @@ class JavascriptCrawler(BaseCrawler):
                 return true;
             }
           """)
-
+        
         for text in path:
             logging.debug("Navigating to %s...", text)
+            #Find and click on element with text
             found = self._click_on_text(text)
             if not found:
                 raise MarkupError(
@@ -178,21 +219,24 @@ def print_table(table, teams):
         table (dict): source => team => score mapping
         teams (list): teams to print.
     """
-
     # Print header
     sources = sorted(table.keys())
-    print("{:30} ".format("Team"), end='')
-    for source in sources:
-        print("{:10} ".format(source), end='')
+    dt = datetime.now()
     print()
-    print("-" * 40)
+    print("Information is actual on {}".format(dt.strftime('%A, %d. %B %Y %I:%M%p')))
+    print()
+    print("{:40} ".format("Team"), end='')
+    for source in sources:
+        print("{:20} ".format(source), end='')
+    print()
+    print("-" * 60)
 
     # Print body
     for team in teams:
-        print("{:30} ".format(team), end='')
+        print("{:40} ".format(team), end='')
         for source in sources:
             scores = table[source].get(team, 'NA')
-            print("{:10} ".format(scores), end='')
+            print("{:20} ".format(scores), end='')
         print()
 
     # Print footer
@@ -208,7 +252,15 @@ if __name__ == '__main__':
                         help="Odds updating interval in secodns, default is %(default)s is"
                         )
     parser.add_argument('--team-names', 
-                        default="['Germany', 'France', 'Argentina']",
+                        default=['Germany', 'Iceland', 'Brazil', 'Saudi Arabia', 
+                                 'Australia', 'Senegal', 'Poland', 'Iran', 
+                                 'Morocco', 'Colombia', 'Peru', 'Portugal', 
+                                 'Denmark', 'Russia', 'Sweden', 'Switzerland', 
+                                 'Costa Rica', 'England', 'Japan', 'Egypt', 
+                                 'Spain', 'Panama', 'Argentina', 'South Korea', 
+                                 'Belgium', 'France', 'Uruguay', 'Mexico', 
+                                 'Croatia', 'Nigeria', 'Serbia', 'Tunisia'],
+                        type=list,
                         help="Team names, default is %(default)s"
                         )
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -216,11 +268,12 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.WARNING)
 
     crawlers = {
-        #"SkyBet": SkyBetCrawler(),
+        "SkyBet": SkyBetCrawler(),
         "WilliamHill": WillIamHillCrawler(),
         "PaddyPower": PaddyPowerCrawler()
     }
-
+    
+    #Repeatedly crawl and parse sites, output data. 
     while True:
         table = {}
         all_teams = set()
@@ -236,109 +289,5 @@ if __name__ == '__main__':
         teams = args.team_names or sorted(all_teams)
         print_table(table, teams)
         time.sleep(args.update_interval)
-
-
-    asdfjaskdfj
-
-
-
-    
-    url = "https://www.skybet.com/"
-    #r = requests.get(url)
-    
-    #=============================================== skybet
-    js_func_get_by_text = "window.getElementsByText = (str, tag = 'a') => { "\
-              "return Array.prototype.slice.call(" \
-              "document.getElementsByTagName(tag))." \
-              "filter(el => el.textContent.trim() === str.trim());}"
-        
-    firefox_options = Options()  
-    #firefox_options.add_argument("--headless") 
-    driver = webdriver.Firefox(firefox_options=firefox_options)
-    try:
-        driver.get(url)
-        driver.implicitly_wait(5)
-        res = driver.execute_script(js_func_get_by_text)
-        print(res)
-        driver.execute_script("window.getElementsByText('Football', tag = 'a')[0].click();")
-        driver.implicitly_wait(20)
-        time.sleep(10)
-        driver.execute_script("window.getElementsByText('Competitions', tag = 'a')[0].click();")
-        time.sleep(15)
-        driver.implicitly_wait(20)
-        driver.execute_script("window.getElementsByText('World Cup 2018', tag = 'span')[0].click();")
-        time.sleep(15)
-        driver.implicitly_wait(20)
-        driver.execute_script("window.getElementsByText('Outrights', tag = 'a')[0].click();")
-        
-        time.sleep(15)
-        driver.implicitly_wait(20)
-        driver.execute_script("window.getElementsByText('World Cup 2018 Winner', tag = 'a')[0].click();")
-        
-        
-        dddddddddddddddddddddddd
-        football_link = driver.execute_script("$f = window.getElementsByText('Football', tag = 'a')[0]")
-        driver.execute_script("$f.click()")
-        #football_link[0].click()
-        driver.implicitly_wait(10)
-        comp_link = driver.execute_script("$c = window.getElementsByText('Competitions', tag = 'a')[0]")
-        driver.execute_script("$c.click()")
-        print("!!!!", comp_link)
-        comp_link[0].click()
-        
-        driver.implicitly_wait(20)
-        #World Cup 2018
-        wc_link = driver.execute_script("$p = window.getElementsByText('Premier League', tag = 'span')")
-        driver.execute_script("$p.click()")
-        print("||| ", wc_link)
-        wc_link[0].click()
-        
-        hjhljhljh
-        res = driver.find_elements(By.XPATH, "//a[text()='Football']")
-        football_link = res[0]
-        print(football_link)
-        #driver.SwitchTo().Frame("frameID");
-        football_link.click()
-        #//*[matches(@id, 'sometext\d+_text')]
-        #[matches(.,' doubles ')]
-        driver.implicitly_wait(5)
-        compet = driver.find_elements(By.XPATH, '//a[contains(text(),"Competitions")]')
-        print(compet)
-        compet_link = compet[0]
-        compet_link.click()
-        driver.implicitly_wait(5)
-        wc_link = driver.find_element(By.XPATH, "//.[text()='World Cup 2018']")
-        print(wc_link)
-        wc_link.click()
-    except Exception as ex:
-        #driver.close()
-        raise ex
-    
-
-    #==================================================https://mobile.bet365.com
-    
-    home_url = "https://mobile.bet365.com"
-
-
-    chrome_options = Options()  
-    #chrome_options.add_argument("--headless")  
-    
-    driver = webdriver.Chrome(chrome_options=chrome_options)  
- 
-    
-        
-    driver.get(home_url)
-    print(driver.current_url)
-    driver.wait = WebDriverWait(driver, 15)
-    #print(dir(driver))
-    #print(dir(driver.find_elements_by_tag_name('Head')[0]))
-    site_dom = driver.find_elements_by_class_name('iconLabel')
-    res = driver.find_elements(By.XPATH, "//div[text()='Soccer']")
-    print(res)
-    #print(site_dom.find_elements_by_id('betslipBar'))
-    #p_element = driver.find_element_by_id(id_='intro-text')
-    #print(p_element.text)
-    
-    
     
     
